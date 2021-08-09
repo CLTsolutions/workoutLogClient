e0efa05093e8e4ce71114d04aa60a65cae459528
Chelsey Tschida <chelsey.tschida@outlook.com>
2021-08-09 19:14:34 -0400

    moved login/signup to auth

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,27 +1,27 @@
-import React from 'react'; //don't need
-import {Container, Row, Col} from 'reactstrap'; //bootstrap tools for grid system
-import Login from '../home/Login';
-import Signup from '../home/Signup';
+import React from 'react' //don't need
+import { Container, Row, Col } from 'reactstrap' //bootstrap tools for grid system
+import Login from './Login'
+import Signup from './Signup'
 
 //functional component with no state
 //hold props being passed down as well as the login/signup forms side by side
-const Auth = (props) => {
-    return (
-        <Container className='auth-container'>
-            <Row>
-                <Col md='6'>
-                    {/* This property (prop) allows us to pass token down to signup function
-                        --  not the same as updateToken in App.js (it's a prop associated with a 
+const Auth = props => {
+  return (
+    <Container className='auth-container'>
+      <Row>
+        <Col md='6'>
+          {/* This property (prop) allows us to pass token down to signup function
+                        --  not the same as updateToken in App.js (it's a prop associated with a
                         --  lower component)
                         --Named same to bridge props through the unidirectional flow */}
-                    <Signup updateToken={props.updateToken} />
-                </Col>
-                <Col md='6' className='login-col'>
-                    <Login updateToken={props.updateToken} />
-                </Col>
-            </Row>
-        </Container>
-    )
+          <Signup updateToken={props.updateToken} />
+        </Col>
+        <Col md='6' className='login-col'>
+          <Login updateToken={props.updateToken} />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
-export default Auth;
+export default Auth
---
+++ src/auth/Signup.js
@@ -0,0 +1,73 @@
+//See notes in Login.js
+import React, { useState } from 'react'
+import { Form, FormGroup, Label, Input, Button, FormText } from 'reactstrap'
+
+//props - function is made in App.js because sessionToken is stored in parent and not all over the app
+const Signup = props => {
+  //these State variables allow us to respond to and control the display of the user-typed info
+  //  --into the input fields in our form we return from this component
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  //this function will eventually do a POST for user info and receive the sessionToken back in a res
+  const handleSubmit = event => {
+    event.preventDefault() //prevents page refresh when form is submitted
+    fetch('http://localhost:3000/user/register', {
+      //fetch to server endpoint
+      method: 'POST',
+      body: JSON.stringify({
+        //must match backend
+        user: { username: username, passwordhash: password },
+      }),
+      //lets server know what type of info we are sending it so it can decide if it can handle it
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+    })
+      //returning promise from fetch and calling json()
+      //-- this allows us to return the res into JSON when it revolves.
+      .then(response => response.json())
+      //--function with returned sessionToken in the data object.
+      //resolving .json() promise and taking returned data and calling updateToken
+      .then(data => {
+        props.updateToken(data.sessionToken)
+      })
+  }
+
+  /***********Input onChange**********
+   * Functions are callback responding to onChange event listener inserted into input fields
+   * these functions are called by event handler (we never call them in code, just define them)
+   * they're called with an 'event' object argument (default behavior by any event handler)
+   * digging into these event objects let us grab hold of user's typed input data.
+   */
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      {/* calling handleSubmit function above
+                -- not using parens bc we aren't calling function ourselves (onSubmit handler is)*/}
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label htmlFor='username'></Label>
+          <Input
+            onChange={e => setUsername(e.target.value)}
+            name='username'
+            value={username}
+          />
+          <FormText>Username is required</FormText>
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor='password'></Label>
+          <Input
+            onChange={e => setPassword(e.target.value)}
+            name='password'
+            value={password}
+            type='password'
+          />
+        </FormGroup>
+        <Button type='submit'>Signup</Button>
+      </Form>
+    </div>
+  )
+}
+
+export default Signup
